e8271ec28e21acd4dbedf76431935cf1fa9e48cc
muziga judith <umuzigajudit@gmail.com>
2025-10-03 09:34:35 +0200

    Loop through each author

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ async function mergeAuthorCommentData() {
 
     const authorRes = await fetch('https://api.example.com/authors');
     if (!authorRes.ok) throw new Error('Failed to fetch authors');
-    const authors = await authorRes.json();  
+    const authors = await authorRes.json(); 
+    
+     onst structuredData = await Promise.all(
+      authors.map(async (author) => {
+
+      }
 
 }
   
